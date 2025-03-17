13757605729648508ef65c0ec24a7530bd785268
UB-Reader Deployment <ub-reader-deploy@example.com>
2025-03-17 19:42:18 -0400

    Fix next.config.js syntax error

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-module.exports = nextConfig; 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  basePath: '',
+  async rewrites() {
+    return [
+      {
+        source: '/json/:path*',
+        destination: '/public/json/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
